7d80f8a9d60916bdba41927fd7bc707a01582dad
zaha <lelazahanara@gmail.com>
2020-10-28 15:50:40 +0600

    work practice
    work practice

---
+++ htmlcsspractice/Javascript practice/basic.js
@@ -0,0 +1,76 @@
+// //
+// if (false || false) {
+//   document.write("its true condition");
+// } else {
+//   document.write("its false condition");
+// }
+
+// var count = 0;
+var br = "<br> </br>";
+// while (count < 10) {
+//   document.write("im in while loop until count is grater than 10" + br);
+//   document.write("now count is " + count + br);
+//   count++;
+// }
+// document.write("im outside of loop" + br);
+// document.write("now count is " + count + br);
+
+// var name = 1;
+// while (name <= 10) {
+//   document.write(name * 5);
+
+//   name++;
+// }
+// document.write("im here");
+
+// var nam=5
+// while(nam<=50){
+//     document.write(nam)
+//     nam=nam+5;
+
+// }
+
+// var a=10
+// for(let i=0;i<a;i++){
+//   document.write(i)
+// }
+// for(let i=0;i<10;i++){
+//   if(i==3||i==6){continue}
+//   document.write(i+br)
+//   // if(i==7){continue}
+
+// if(i==7){
+//   break;
+// }
+
+// }
+// var myarray=[];
+// myarray[0]="sia"
+// document.write(myarray+br)
+// document.write(myarray[0]+br)
+// myarray[1]=4
+// document.write(myarray+br)
+// document.write(myarray[1]+br)
+// myarray[3]=7
+// document.write(myarray[3]+br)
+// document.write(myarray+br)
+// for(let i=0;i<myarray.length;i++){
+
+//   document.write(myarray[i]+br)
+
+// }
+// var names=["sia","naila","sina","nipal"]
+// for(let i=0;i<names.length;i++){
+//   document.write(names[i]+br)
+//   if(names[i]=="naila"){
+//     continue;
+//   }
+// }
+
+// var divcount=document.getElementsByTagName("div")
+// console.log(divcount)
+
+// for(let i=0; i<divcount.length; i++){
+// console.log(divcount[i])
+// divcount[i].className+="dj tj"
+// }
